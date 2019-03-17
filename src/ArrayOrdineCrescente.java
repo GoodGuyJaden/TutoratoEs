6fd8b079002ca6a43c1c6fb211acb2033b37a6eb
mazza <mazza@DESKTOP-Q01HKSI>
2019-03-17 18:06:56 +0100

    esercizio 1

---
+++ src/ArrayOrdineCrescente.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+
+public class ArrayOrdineCrescente 
+{
+	public static void main(String[] args) 
+	{
+		int [] a = {7, 9, 12, 27, 0, 1, 5};
+		int v = 0;
+		
+		for(int i = 0, j = 0; i < a.length; j++)
+		{
+			if(a[i] > a[j])
+			{
+				v = a[i];
+				a[i] = a[j];
+				a[j] = v;
+			}
+			
+			if(j == a.length-1)
+			{
+				i++;
+				j = i-1;;
+			}
+		}
+		
+		System.out.println(Arrays.toString(a));
+	
+	}
+}
